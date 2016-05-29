2e60fd86d3a559c7e3f3714ec38d3aa58f28a9de
jeffei <jeffeicheung@gmail.com>
2016-05-29 19:39:24 +0800

    modify style

--- Sort/bubbleSortForList.cpp
+++ Sort/bubbleSortForList.cpp
@@ -2,7 +2,8 @@ void bubbleSortForList(ListNode* head){
 	if(!head||!head->next) return;
 	for(ListNode* i=head;i->next;i=i->next){
 		for(ListNode* j=head;j->next;j=j->next){
-			if(j->val>j->next->val) swap(j->val,j->next->val);
+			if(j->val>j->next->val) 
+				swap(j->val,j->next->val);
 		}
 	}
 }
